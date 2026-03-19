dcf92e139298bcf38f18965fc40db22695fafe88
Xharles <charlesnzubechukwu000@gmail.com>
2026-03-19 14:20:04 +0100

    feat: implement routing in App component and wrap with BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Problem from "./components/Problem";
-// import Features from "./components/Features";
-// import Manifesto from "./components/Manifesto";
 import HowItWorks from "./components/HowItWorks";
 import Metrics from "./components/Metrics";
 import CTABlock from "./components/CTABlock";
@@ -10,6 +9,30 @@ import RecipeBuilder from "./components/RecipeBuilder";
 import PopularRecipes from "./components/PopularRecipes";
 import Footer from "./components/Footer";
 import { AuthModal } from "./components/auth/AuthModal";
+import { Dashboard } from "./pages/Dashboard";
+// import { PriceAlert } from "./pages/alerts/PriceAlert";
+// import { PercentAlert } from "./pages/alerts/PercentAlert";
+// import { PeriodicAlert } from "./pages/alerts/PeriodicAlert";
+// import { VolumeAlert } from "./pages/alerts/VolumeAlert";
+// import { FundingRateAlert } from "./pages/alerts/FundingRateAlert";
+// import { MarketCapAlert } from "./pages/alerts/MarketCapAlert";
+// import { DominanceAlert } from "./pages/alerts/DominanceAlert";
+// import { StockAlert } from "./pages/alerts/StockAlert";
+// import { CustomAlert } from "./pages/alerts/CustomAlert";
+
+function HomePage() {
+  return (
+    <>
+      <Hero />
+      <Problem />
+      <HowItWorks />
+      <Metrics />
+      <CTABlock />
+      <RecipeBuilder />
+      <PopularRecipes />
+    </>
+  );
+}
 
 function App() {
   return (
@@ -17,15 +40,19 @@ function App() {
       <Navbar />
       <AuthModal />
       <div className="flex-1 w-full flex flex-col">
-        <Hero />
-        <Problem />
-        {/* <Features /> */}
-        {/* <Manifesto /> */}
-        <HowItWorks />
-        <Metrics />
-        <CTABlock />
-        <RecipeBuilder />
-        <PopularRecipes />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          {/* <Route path="/alerts/price" element={<PriceAlert />} />
+          <Route path="/alerts/percent" element={<PercentAlert />} />
+          <Route path="/alerts/periodic" element={<PeriodicAlert />} />
+          <Route path="/alerts/volume" element={<VolumeAlert />} />
+          <Route path="/alerts/funding-rate" element={<FundingRateAlert />} />
+          <Route path="/alerts/market-cap" element={<MarketCapAlert />} />
+          <Route path="/alerts/dominance" element={<DominanceAlert />} />
+          <Route path="/alerts/stock" element={<StockAlert />} />
+          <Route path="/alerts/custom" element={<CustomAlert />} /> */}
+        </Routes>
       </div>
       <Footer />
     </div>
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 import './index.css'
 import App from './App.tsx'
@@ -8,7 +9,9 @@ import { OAUTH } from './api/config'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <GoogleOAuthProvider clientId={OAUTH.GOOGLE_CLIENT_ID}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </GoogleOAuthProvider>
   </StrictMode>,
 )
